6d4c9449270469a8e27718d72e7416d6a0f2299f
rajnish6191 <onlinerajnish@yahoo.com>
2016-03-18 23:46:48 -0700

    project

---
+++ project/src/main/java/org/utils/WordCount.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.utils;
+import java.io.IOException;
+import  java.util.StringTokenizer;
+
+public class WordCount {
+
+    
+    
+    public  static class  TokenizerMapper extends Object{
+       public  void  map(String Object_Key, String Text_value, String Context_contextwrite)throws IOException, InterruptedException{
+           
+         StringTokenizer itr = new StringTokenizer("value");
+         while(itr.hasMoreTokens()){
+        
+          Context_contextwrite.contentEquals("word, one");
+             
+         }
+        
+          
+       }
+    }
+    public static void main(String[] args) {
+       }
+    
+}
